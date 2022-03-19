a6483dcaf4ce551d839e95b0170f0f926cffbbc0
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-03-18 20:47:52 -0400

    update config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
-from transformers import BertTokenizer
 from pathlib import Path
+
+import torch
+from transformers import BertTokenizer
 
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
 ACCUMULATION = 2
-BERT_PATH = Path("../input/bert_base_uncased")
+# BERT_PATH = Path("../input/bert_base_uncased")
 MODEL_PATH = "model.bin"
-TRAINING_FILE = Path('../data/train-data.csv')
-TESTING_FILE = Path('../data/test-data.csv')
-TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
- 
+TRAINING_FILE = Path("./data/train-data.csv")
+TESTING_FILE = Path("./data/test-data.csv")
+TOKENIZER = BertTokenizer.from_pretrained("bert-base-uncased")
+SCALER = torch.cuda.amp.GradScaler()
